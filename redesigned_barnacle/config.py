e567d107afd7f05c5d8c844568da0c68c8400a1d
ssube <seansube@gmail.com>
2020-02-08 19:37:02 -0600

    feat(config): add load and parse helper

--- redesigned_barnacle/config.py
+++ redesigned_barnacle/config.py
@@ -1,22 +1,37 @@
+import os
+
+
+def load_config(path, name):
+    files = os.listdir(path)
+    if not name in files:
+        raise Exception('config file missing')
+
+    config = parse_file('{}/{}'.format(path, name))
+    print('Config: {}'.format(config))
+
+    return config
+
+
 def parse_file(name, open=open):
-  with open(name, 'r') as source:
-    return parse_str(source.readlines())
+    with open(name, 'r') as source:
+        return parse_str(source.readlines())
+
 
 def parse_str(lines):
-  data = {}
-  for rline in lines:
-    line = rline.strip()
-    if len(line) == 0 or line[0] == '#':
-      continue
+    data = {}
+    for rline in lines:
+        line = rline.strip()
+        if len(line) == 0 or line[0] == '#':
+            continue
 
-    parts = line.split(':')
-    if len(parts) < 2:
-      continue
+        parts = line.split(':')
+        if len(parts) < 2:
+            continue
 
-    value = ''.join(parts[1:]).strip()
-    if len(value) == 0:
-      continue
+        value = ''.join(parts[1:]).strip()
+        if len(value) == 0:
+            continue
 
-    data[parts[0]] = value
+        data[parts[0]] = value
 
-  return data
+    return data
