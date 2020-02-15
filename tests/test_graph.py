a080fadfb596780a738d7770e57077d3d3aecfd6
ssube <seansube@gmail.com>
2020-02-14 18:37:09 -0600

    fix(Tests): provide buffer to graph

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -1,3 +1,4 @@
+from redesigned_barnacle.buffer import CircularBuffer
 from redesigned_barnacle.graph import Sparkline
 from redesigned_barnacle.mock import MockFramebuffer
 from unittest import TestCase
@@ -5,6 +6,7 @@ from unittest import TestCase
 
 class SparkTest(TestCase):
   def test_line(self):
-    sl = Sparkline(32, 64)
+    buf = CircularBuffer()
+    sl = Sparkline(32, 64, buf)
     sl.push(16)
     sl.draw(MockFramebuffer(), 0, 0)
