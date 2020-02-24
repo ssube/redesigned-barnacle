914582037ce08b47fe3e5388af7b519b2b70819f
ssube <seansube@gmail.com>
2020-02-24 08:50:08 -0600

    fix(scripts): copy code to correct card paths

--- scripts/setup-card.sh
+++ scripts/setup-card.sh
@@ -11,7 +11,7 @@ IMAGE_NAME="${3:-esp32_poe}"
 BOARD_PORT="${4:-/usr/ttyUSB0}"
 
 cp -v ${ROOT_PATH}/config/${CONFIG_NAME}.yml ${CARD_PATH}/config.yml
-rsync -avhL ${ROOT_PATH}/image/app ${CARD_PATH}/app
-rsync -avhL ${ROOT_PATH}/image/lib ${CARD_PATH}/lib
+rsync -avhL ${ROOT_PATH}/image/app ${CARD_PATH}/
+rsync -avhL ${ROOT_PATH}/image/lib ${CARD_PATH}/
 
 # ampy -p ${BOARD_PORT} -b 115200 put ./image/boot.py /boot.py
